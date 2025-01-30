ec599b34927d4d3721eeed353907d73841e636c0
GamesMuch <everydaycrevan@gmail.com>
2025-01-30 19:29:40 +0100

    hlep

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -6,17 +6,6 @@ using UnityEngine.UIElements;
 public class NewBehaviourScript : MonoBehaviour
 {
     public int SpinSpeed = 5;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void FixedUpdate()
     {
         gameObject.transform.Rotate(0, SpinSpeed * Time.deltaTime, 0);
